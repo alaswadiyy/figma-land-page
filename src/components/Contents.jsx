eeff9673b68bb59d1e38d7e582c28e68c62484fa
Abdulmuiz Sharafadeen <abdulmuizsharafadeen@gmail.com>
2023-12-09 15:24:30 +0100

    feat: content section

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,9 +1,31 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/jsx-no-comment-textnodes */
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 
 const Contents = () => {
   return (
-    <div>Contents</div>
+    <section>
+      <div>
+        <h2>Contents</h2>
+      </div>
+      <div>
+        // eslint-disable-next-line react/no-unescaped-entities
+        <p>We focus on ergonomics and meeting you where you work. It's only a keystroke away.</p>
+      </div>
+      <div>
+        <div>
+          <h4>Work</h4>
+          <p>Ever wondered if you're too reliant on a client for work? Slate helps you identify .</p>
+          <button>Sign Up</button>
+        </div>
+        <div>
+          <h4>Design with real data</h4>
+          <p>Ever wondered if you're too reliant on a client for work? Slate helps you identify .</p>
+          <button>Try For Free</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
